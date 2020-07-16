6e231a382999e4e175a0b00e4c92250d4b0862a0
Bolun Thompson <abolunthompson@gmail.com>
2020-07-15 18:07:33 -0700

    Reformatted and refactored code into the export_contribution_information_to_json function, acting like a main function

--- scripts/export_contribution_info_to_json.py
+++ scripts/export_contribution_info_to_json.py
@@ -7,23 +7,24 @@ EXPENDITURE_NAMES = ["D-Expenditure", "G-Expenditure", "E-Expenditure"]
 
 
 def load_sheets_from_years(years, sheet_names):
-    '''
+    """
     Returns the specified sheets of the given year(s) (e.g 2020).
     A list of the years and the sheet names are the argruments
-    '''
+    """
     sheets = []
     for year in years:
         for sheet in sheet_names:
-            sheets.append(pandas.read_excel("static/efile_SD_CSD_"
-                                            + str(year)+".xlsx", sheet))
+            sheets.append(
+                pandas.read_excel("static/efile_SD_CSD_" + str(year) + ".xlsx", sheet)
+            )
     return sheets
 
 
 def sum_of_sheet_columns(summed_column, *args):
-    '''
+    """
     returns sum of columns of the specified name on specified sheets
     Takes *args of sheets and a string of the column name
-    '''
+    """
     total = 0
     for sheet in args:
         total += sheet[summed_column].sum()
@@ -31,44 +32,53 @@ def sum_of_sheet_columns(summed_column, *args):
 
 
 def unique_sheet_columns_sum(identifier_column, summed_column, *args):
-    '''
+    """
     returns pandas series with the sum each identifier's column
     combined from every sheet.Takes *args of sheets and a string of
     the column name and the identifier column.
-    '''
+    """
     dict_totals = {}
     for sheet in args:
         for c, identifier in enumerate(sheet[identifier_column]):
             if dict_totals.get(identifier) is None:
                 dict_totals.update({identifier: sheet[summed_column].iloc[c]})
             else:
-                num = dict_totals.get(identifier)+sheet[summed_column].iloc[c]
+                num = dict_totals.get(identifier) + sheet[summed_column].iloc[c]
                 dict_totals.update({identifier: num})
     return pandas.Series(dict_totals)
 
 
-if __name__ == '__main__':
+def export_contribution_info_to_json():
     contribution_sheets = load_sheets_from_years([2019, 2020], CONTRIBUTION_NAMES)
     expenditure_sheets = load_sheets_from_years([2019, 2020], EXPENDITURE_NAMES)
 
     total_contributions = sum_of_sheet_columns("Tran_Amt2", *contribution_sheets)
     total_expenditures = sum_of_sheet_columns("Amount", *expenditure_sheets)
-    expenditures_by_zip = unique_sheet_columns_sum("Tran_Zip4", "Tran_Amt2",
-                                                   *contribution_sheets)
-    expenditures_by_occupation = unique_sheet_columns_sum("Tran_Occ", "Tran_Amt2",
-                                                          *contribution_sheets)
+    expenditures_by_zip = unique_sheet_columns_sum(
+        "Tran_Zip4", "Tran_Amt2", *contribution_sheets
+    )
+    expenditures_by_occupation = unique_sheet_columns_sum(
+        "Tran_Occ", "Tran_Amt2", *contribution_sheets
+    )
 
     if not path.exists("raw/"):
         makedirs("raw/")
 
     expenditures_by_zip.to_json("raw/expenditures_by_zip.json")
-    expenditures_by_occupation.to_json("raw/expenditures_by_occupation.json",
-                                       orient="columns")
+    expenditures_by_occupation.to_json(
+        "raw/expenditures_by_occupation.json", orient="columns"
+    )
 
-    series_total_contributions = pandas.Series(total_contributions,
-                                               index=["total_contributions"])
+    series_total_contributions = pandas.Series(
+        total_contributions, index=["total_contributions"]
+    )
     series_total_contributions.to_json("raw/total_contributions.json")
 
-    series_total_expenditures = pandas.Series(total_expenditures,
-                                              index=["total_expenditures"])
+    series_total_expenditures = pandas.Series(
+        total_expenditures, index=["total_expenditures"]
+    )
     series_total_expenditures.to_json("raw/total_expenditures.json")
+
+
+if __name__ == "__main__":
+    export_contribution_info_to_json()
