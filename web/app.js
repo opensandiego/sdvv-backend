725897d17f60565f87d3dcdd755b0b4f4cc51fe4
Robert <robertgz@users.noreply.github.com>
2021-01-12 09:33:06 -0800

    Updated Docker setup for Heroku

--- web/app.js
+++ web/app.js
@@ -2,5 +2,5 @@ const express = require('express');
 const PORT = process.env.PORT || 5000;
 
 express()
-  .get('/', (req, res) => res.send('Hello San Diego Voters Voice!'))
+  .get('/', (req, res) => res.send('Hello San Diego Voters Voice, in Docker!'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
