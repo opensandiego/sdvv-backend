94dc8890e1f3521ddce692962faeb4cd7f4730fc
Robert <robertgz@users.noreply.github.com>
2021-01-12 16:21:10 -0800

    Updated docker setup

--- web/app.js
+++ web/app.js
@@ -2,5 +2,5 @@ const express = require('express');
 const PORT = process.env.PORT || 5000;
 
 express()
-  .get('/', (req, res) => res.send('Hello San Diego Voters Voice, in Docker, with postgresql and redis addons!'))
+  .get('/', (req, res) => res.send('Hello San Diego Voters Voice!'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
