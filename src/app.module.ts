19fb73c758b0bc15371b4ce34595c1ac0b8c9ef3
Robert <robertgz@users.noreply.github.com>
2021-08-21 15:05:07 -0700

    improved database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,34 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ElectionsController } from './elections/elections.controller';
-import { CandidatesController } from './candidates/candidates.controller';
-import { ChartDataController } from './chart-data/chart-data.controller';
 
-import { Connection } from 'typeorm';
+import { TypeOrmConfigService } from './config/database.config';
 import { ElectionsModule } from './elections/elections.module';
 import { CandidatesModule } from './candidates/candidates.module';
 import { FilingsModule } from './filings/filings.module';
 import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  // imports: [TypeOrmModule.forRoot(), ElectionsModule],
   imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
     ElectionsModule,
     CandidatesModule,
     FilingsModule,
     TransactionsModule,
   ],
-  controllers: [
-    AppController,
-    ElectionsController,
-    CandidatesController,
-    ChartDataController,
-  ],
+  controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-  // constructor(private connection: Connection) {}
-}
+export class AppModule {}
---
+++ src/config/database.config.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT || 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: true,
+      autoLoadEntities: true,
+    };
+  }
+}
