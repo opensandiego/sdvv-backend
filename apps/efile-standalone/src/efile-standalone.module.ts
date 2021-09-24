e3372e199ac80bdd2b1ad859b08e84df4741d7cb
Robert <robertgz@users.noreply.github.com>
2021-09-23 17:05:16 -0700

    Changed EfileStandaloneModule from a web app to a standalone app so than it can run in a worker process in Heroku.

--- apps/efile-standalone/src/efile-standalone.module.ts
+++ apps/efile-standalone/src/efile-standalone.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { EfileStandaloneController } from './efile-standalone.controller';
 import { EfileStandaloneService } from './efile-standalone.service';
 
 @Module({
   imports: [],
-  controllers: [EfileStandaloneController],
   providers: [EfileStandaloneService],
 })
 export class EfileStandaloneModule {}
--- apps/efile-standalone/src/main.ts
+++ apps/efile-standalone/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { EfileStandaloneModule } from './efile-standalone.module';
+import { EfileStandaloneService } from './efile-standalone.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(EfileStandaloneModule);
-  await app.listen(3000);
+  const app = await NestFactory.createApplicationContext(EfileStandaloneModule);
+  const tasksService = app.get(EfileStandaloneService);
+  console.log(tasksService.getHello());
 }
 bootstrap();
