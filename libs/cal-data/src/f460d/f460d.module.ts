d18891ada28e0195851500b5e0025caa57bcf6f7
Robert <robertgz@users.noreply.github.com>
2021-09-23 23:12:07 -0700

    Imported SharedModule into F460DModule

--- libs/cal-data/src/f460d/f460d.module.ts
+++ libs/cal-data/src/f460d/f460d.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { SharedModule } from '../shared/shared.module';
 import { F460DEntity } from './f460d.entity';
 import { F460DService } from './f460d.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([F460DEntity])],
+  imports: [TypeOrmModule.forFeature([F460DEntity]), SharedModule],
   providers: [F460DService],
   exports: [F460DService],
 })
--- libs/cal-data/src/f460d/f460d.service.ts
+++ libs/cal-data/src/f460d/f460d.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Connection, Repository } from 'typeorm';
+import { SharedService } from '../shared/shared.service';
+import { CreateF460DDto } from './dto/createF460D.dto';
 import { F460DEntity } from './f460d.entity';
 
 @Injectable()
@@ -8,12 +10,17 @@ export class F460DService {
   constructor(
     @InjectRepository(F460DEntity)
     private f460dRepository: Repository<F460DEntity>,
-    private connection: Connection,
+    // private connection: Connection,
+    private sharedService: SharedService,
   ) {}
 
   async findAll(): Promise<F460DEntity[]> {
     console.log('f460dRepository findAll');
 
     return await this.f460dRepository.find();
+  }
+
+  async createBulkF460D(createF460DDto: CreateF460DDto[]) {
+    await this.sharedService.createBulkData(createF460DDto, F460DEntity);
   }
 }
