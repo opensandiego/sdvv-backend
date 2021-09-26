3e945c0135eb970f83962d2a7e69fcc43844050c
Robert <robertgz@users.noreply.github.com>
2021-09-26 13:40:12 -0700

    Removed queue processor from ZipCodeCSVModule

--- apps/standalone-worker/src/zip.code.csv/zip.code.csv.module.ts
+++ apps/standalone-worker/src/zip.code.csv/zip.code.csv.module.ts
@@ -1,19 +1,11 @@
 import { ZipCodesModule } from '@app/cal-data/zipCodes/zipCodes.module';
-import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { UtilsModule } from '../utils/utils.module';
-import { ZipCodeCSVProcessor } from './zip.code.csv.processor.service';
 import { ZipCodeCSVService } from './zip.code.csv.service';
 
 @Module({
-  imports: [
-    BullModule.registerQueue({
-      name: 'zip-csv-tasks',
-    }),
-    UtilsModule,
-    ZipCodesModule,
-  ],
-  providers: [ZipCodeCSVService, ZipCodeCSVProcessor],
-  exports: [ZipCodeCSVService, ZipCodeCSVProcessor],
+  imports: [UtilsModule, ZipCodesModule],
+  providers: [ZipCodeCSVService],
+  exports: [ZipCodeCSVService],
 })
 export class ZipCodeCSVModule {}
