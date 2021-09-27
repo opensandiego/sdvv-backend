215ff54b4f6b9a34c5b4b8485d8c82c9a0e82bb0
Robert <robertgz@users.noreply.github.com>
2021-09-26 23:55:49 -0700

    moved imports

--- apps/standalone-worker/src/standalone-worker.module.ts
+++ apps/standalone-worker/src/standalone-worker.module.ts
@@ -3,6 +3,7 @@ import { BullModule } from '@nestjs/bull';
 import { HttpModule } from '@nestjs/axios';
 import { DatabaseModule } from '@app/sdvv-database';
 import { StandaloneWorkerService } from './standalone-worker.service';
+import { QueueDispatchModule } from './queue.dispatch/queue.dispatch.module';
 import { TransactionsXLSXModule } from './transactions.xlsx/transactions.xlsx.module';
 import { F460DModule } from '@app/sdvv-database/f460d/f460d.module';
 import { ZipCodeCSVModule } from './zip.code.csv/zip.code.csv.module';
