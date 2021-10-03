84ef6d605803ffbc4322f50983b7ff5ec1ffb4e1
Robert <robertgz@users.noreply.github.com>
2021-10-03 13:05:08 -0700

    Updated CandidateIndependentExpendituresService

--- libs/efile-api-data/src/queries/candidate.independent.expenditures.service.ts
+++ libs/efile-api-data/src/queries/candidate.independent.expenditures.service.ts
@@ -6,31 +6,51 @@ import { CalculationTransaction } from '../tables/entity/calculation.transaction
 export class CandidateIndependentExpendituresService {
   constructor(private connection: Connection) {}
 
-  async support(candidateName: string) {
-    const { sum: supportSum } = await this.connection
-      .getRepository(CalculationTransaction)
-      .createQueryBuilder()
-      .select('SUM(amount)', 'sum')
-      .where('name = :candidateName', { candidateName: candidateName })
-      .andWhere('tx_type = :txType', { txType: 'EXPN' })
-      .andWhere('spending_code = :spendingCode', { spendingCode: 'IND' })
-      .andWhere('sup_opp_cd = :supOppCd', { supOppCd: 'SUPPORT' })
-      .getRawOne();
-
-    return supportSum;
+  async support(candidateName: string, electionDate: string) {
+    return this.getIndependentExpenditures(
+      candidateName,
+      'SUPPORT',
+      electionDate,
+    );
   }
 
-  async opposed(candidateName: string) {
-    const { sum: opposeSum } = await this.connection
+  async opposed(candidateName: string, electionDate: string) {
+    return this.getIndependentExpenditures(
+      candidateName,
+      'OPPOSE',
+      electionDate,
+    );
+  }
+
+  async getIndependentExpenditures(
+    candidateName: string,
+    supOppCd: string,
+    electionDate: string,
+  ) {
+    const { sum: expSum } = await this.connection
       .getRepository(CalculationTransaction)
       .createQueryBuilder()
       .select('SUM(amount)', 'sum')
-      .where('name = :candidateName', { candidateName: candidateName })
-      .andWhere('tx_type = :txType', { txType: 'EXPN' })
-      .andWhere('spending_code = :spendingCode', { spendingCode: 'IND' })
-      .andWhere('sup_opp_cd = :supOppCd', { supOppCd: 'OPPOSE' })
+      .where('name iLike :candidateName', {
+        candidateName: `%${candidateName}%`,
+      })
+      .andWhere('tx_type = :txType', { txType: 'EXPN' })
+      .andWhere('spending_code = :spendingCode', { spendingCode: 'IND' })
+      .andWhere('sup_opp_cd = :supOppCd', { supOppCd: supOppCd })
+      .andWhere(
+        `to_date(transaction_date, 'MM/DD/YYYY') >= to_date(:electionDate, 'MM/DD/YYYY') - interval '16 month'`,
+        {
+          electionDate,
+        },
+      )
+      .andWhere(
+        `to_date(transaction_date, 'MM/DD/YYYY') <= to_date(:electionDate , 'MM/DD/YYYY')`,
+        {
+          electionDate,
+        },
+      )
       .getRawOne();
 
-    return opposeSum;
+    return expSum ? expSum : 0;
   }
 }
