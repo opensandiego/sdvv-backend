c29a35b84e4d17d9cdfbe3411d6214624fc6a93d
Robert <robertgz@users.noreply.github.com>
2021-11-28 09:30:59 -0800

    Ran npm install --save nest-winston winston Added winston to StandaloneWorkerModule.

--- apps/standalone-worker/src/standalone-worker.module.ts
+++ apps/standalone-worker/src/standalone-worker.module.ts
@@ -12,6 +12,11 @@ import { ZipCodeCSVModule } from './zip.code.csv/zip.code.csv.module';
 import { EFileApiModule } from './efile.api/efile.api.module';
 import { ProcessDataModule } from '@app/sdvv-database/process.data/process.data.module';
 import { EfileApiDataModule } from '@app/efile-api-data';
+import {
+  utilities as nestWinstonModuleUtilities,
+  WinstonModule,
+} from 'nest-winston';
+import * as winston from 'winston';
 
 @Module({
   imports: [
@@ -26,6 +31,19 @@ import { EfileApiDataModule } from '@app/efile-api-data';
     }),
     BullModule.forRoot({
       redis: process.env.REDIS_URL,
+    }),
+    WinstonModule.forRoot({
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.ms(),
+            nestWinstonModuleUtilities.format.nestLike('Worker', {
+              prettyPrint: true,
+            }),
+          ),
+        }),
+      ],
     }),
     HttpModule,
     QueueDispatchModule,
