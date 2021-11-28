8310ca65a729c7538e3203cdd81c1bcddfb50456
Robert <robertgz@users.noreply.github.com>
2021-11-28 10:31:24 -0800

    Updated ClassValidationService to use logger.

--- apps/standalone-worker/src/utils/utils.class.validation.service.ts
+++ apps/standalone-worker/src/utils/utils.class.validation.service.ts
@@ -1,21 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { plainToClass } from 'class-transformer';
 import { validate } from 'class-validator';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
 
 @Injectable()
 export class ClassValidationService {
+  constructor(
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+  ) {}
+
   async getValidatedClasses(objects, dto): Promise<typeof dto[]> {
     const classes = plainToClass(dto, objects);
 
     const errors = await this.validateArray(classes);
 
     if (errors.length > 0) {
-      console.log('validation failed.');
-      // console.log('validation errors: ', errors);
-      console.log('validation error count: ', errors.length);
-      return [];
-    } else {
-      console.log('validation succeed');
+      this.logger.log({
+        level: 'error',
+        message: 'Class validation failed',
+        errorCount: errors.length,
+      });
+
+      throw 'Class validation failed';
     }
 
     return classes;
