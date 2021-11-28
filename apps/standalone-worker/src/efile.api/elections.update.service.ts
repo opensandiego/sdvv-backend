8e8338b3f179f834ccfb3795e7d2c2c48060857f
Robert <robertgz@users.noreply.github.com>
2021-11-28 09:33:17 -0800

    Added winston Logger to ElectionsUpdateService.

--- apps/standalone-worker/src/efile.api/elections.update.service.ts
+++ apps/standalone-worker/src/efile.api/elections.update.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
 import { ClassValidationService } from '../utils/utils.class.validation.service';
 import { CreateElectionDto } from '@app/efile-api-data/tables/dto/createElection.dto';
 import { ElectionEntity } from '@app/efile-api-data/tables/entity/elections.entity';
 import { SharedService } from '@app/sdvv-database/shared/shared.service';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
 
 @Injectable()
 export class ElectionsUpdateService {
@@ -12,6 +14,7 @@ export class ElectionsUpdateService {
     private httpService: HttpService,
     private classValidationService: ClassValidationService,
     private sharedService: SharedService,
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
   ) {}
 
   private eFileElectionUrl =
