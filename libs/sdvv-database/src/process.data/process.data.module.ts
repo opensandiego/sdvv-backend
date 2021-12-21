54ea6e88b7ceede1cd2e771c01b2a89decd4f60c
Robert <robertgz@users.noreply.github.com>
2021-12-21 15:10:56 -0800

    Removed FilingTransactionService. Removed UpdateIndepExpnService.

--- libs/sdvv-database/src/process.data/process.data.module.ts
+++ libs/sdvv-database/src/process.data/process.data.module.ts
@@ -1,24 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TransactionEntity } from '@app/efile-api-data/tables/entity/transactions.entity';
-import { FilingTransactionService } from './filing.transaction.service';
 import { CandidateCommitteeService } from './candidate.committee.service';
-import { UpdateIndepExpnService } from './update.indep.expn.service';
 import { CandidateYearService } from './candidates.year.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([TransactionEntity])],
-  providers: [
-    UpdateIndepExpnService,
-    CandidateCommitteeService,
-    FilingTransactionService,
-    CandidateYearService,
-  ],
-  exports: [
-    UpdateIndepExpnService,
-    CandidateCommitteeService,
-    FilingTransactionService,
-    CandidateYearService,
-  ],
+  providers: [CandidateCommitteeService, CandidateYearService],
+  exports: [CandidateCommitteeService, CandidateYearService],
 })
 export class ProcessDataModule {}
