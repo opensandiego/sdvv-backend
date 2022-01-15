338483d4544a05b90ef5677a26a36bb7ace332df
Robert <robertgz@users.noreply.github.com>
2022-01-15 14:00:09 -0800

    Added GraphQL packages. Added the configuration in GraphQLSetupModule.

---
+++ libs/sdvv-database/src/graphql-setup.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+      },
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+  ],
+})
+export class GraphQLSetupModule {}
