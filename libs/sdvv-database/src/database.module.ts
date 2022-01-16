0bcbf53f41f782fe6d5b2ed99c5c3a14a9654e67
Robert <robertgz@users.noreply.github.com>
2022-01-15 17:00:17 -0800

    Added GraphQLSetupModule to DatabaseModule.

--- libs/sdvv-database/src/database.module.ts
+++ libs/sdvv-database/src/database.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DatabaseService } from './database.service';
 import { ZipCodesModule } from './zipCodes/zipCodes.module';
 import { JurisdictionsModule } from './jurisdictions/jurisdictions.module';
+import { GraphQLSetupModule } from './graphql-setup.module';
 
 @Module({
-  imports: [ZipCodesModule, JurisdictionsModule],
+  imports: [ZipCodesModule, JurisdictionsModule, GraphQLSetupModule],
   providers: [DatabaseService],
   exports: [DatabaseService],
 })
