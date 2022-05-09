ccc06c11e7399aa5777990ed70e38deabeae6f97
Robert <robertgz@users.noreply.github.com>
2022-05-08 17:48:14 -0700

    Removed obsolete REST API and database queries

--- apps/sdvv-backend-nest/src/app.module.ts
+++ apps/sdvv-backend-nest/src/app.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BullModule } from '@nestjs/bull';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import type { ClientOpts as RedisClientOpts } from 'redis';
+import * as redisStore from 'cache-manager-redis-store';
 import { join } from 'path';
 
 import { AppController } from './app.controller';
@@ -10,8 +12,9 @@ import { AppService } from './app.service';
 
 import { getConnectionOptions } from 'typeorm';
 import { EfileApiDataModule } from '@app/efile-api-data';
-import { APIModule } from './api/api.module';
 import { DatabaseModule } from '@app/sdvv-database';
+import { RCPTModule } from '@app/sdvv-database/tables-xlsx/rcpt/rcpt.module';
+import { EXPNModule } from '@app/sdvv-database/tables-xlsx/expn/expn.module';
 
 @Module({
   imports: [
@@ -30,14 +33,19 @@ import { DatabaseModule } from '@app/sdvv-database';
     BullModule.forRoot({
       redis: process.env.REDIS_URL,
     }),
+    // CacheModule.register<RedisClientOpts>({
+    //   store: redisStore,
+    //   url: process.env.REDIS_URL,
+    //   // In production set cache to 6 hours = 21600 seconds
+    //   ttl: process.env.NODE_ENV === 'production' ? 21600 : 10,
+    // }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'sdvv-backend-nest/public'),
     }),
     EfileApiDataModule,
     DatabaseModule,
-    APIModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, RCPTModule, EXPNModule],
 })
 export class AppModule {}
