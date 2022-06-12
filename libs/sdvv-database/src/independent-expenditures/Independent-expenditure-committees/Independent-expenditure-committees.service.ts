cdf406bf353c121fac42ec642abf8f2197d2ee33
Robert <robertgz@users.noreply.github.com>
2022-06-12 13:47:46 -0700

    GraphQL query for Independent Expenditures now include late expenditures but excludes those already included in regular expenditures.

--- libs/sdvv-database/src/independent-expenditures/Independent-expenditure-committees/Independent-expenditure-committees.service.ts
+++ libs/sdvv-database/src/independent-expenditures/Independent-expenditure-committees/Independent-expenditure-committees.service.ts
@@ -1,6 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { Connection } from 'typeorm';
 import { EXPNEntity } from '../../tables-xlsx/expn/expn.entity';
+import { S496Entity } from '@app/sdvv-database/tables-xlsx/s496/s496.entity';
+
+interface Committee {
+  name: string;
+}
+export interface IndependentExpenditureCommittee {
+  committeeName: string;
+  committee: Committee;
+  sum: number;
+}
 
 @Injectable()
 export class IndependentExpenditureCommitteesService {
@@ -14,31 +24,82 @@ export class IndependentExpenditureCommitteesService {
     return `${this.monthDay}/${year}`;
   }
 
-  async supportCommittees(candidateName: string, electionYear: string) {
-    const yearEndDate = `${this.monthDay}/${electionYear}`;
-
-    return this.getIndependentExpenditureCommittees(
-      candidateName,
-      'SUPPORT',
-      yearEndDate,
-    );
+  async supportCommittees(
+    candidateName: string,
+    electionYear: string,
+  ): Promise<IndependentExpenditureCommittee[]> {
+    return this.getBothCommittees(candidateName, 'SUPPORT', electionYear);
   }
 
-  async opposeCommittees(candidateName: string, electionYear: string) {
+  async opposeCommittees(
+    candidateName: string,
+    electionYear: string,
+  ): Promise<IndependentExpenditureCommittee[]> {
+    return this.getBothCommittees(candidateName, 'OPPOSE', electionYear);
+  }
+
+  async getBothCommittees(
+    candidateName: string,
+    supOppCd: string,
+    electionYear: string,
+  ): Promise<IndependentExpenditureCommittee[]> {
     const yearEndDate = `${this.monthDay}/${electionYear}`;
 
-    return this.getIndependentExpenditureCommittees(
+    const f460Committees = await this.getIndependentExpenditureCommittees(
       candidateName,
-      'OPPOSE',
+      supOppCd,
       yearEndDate,
     );
+
+    const f496Committees = await this.getLateIndependentExpenditureCommittees(
+      candidateName,
+      supOppCd,
+      yearEndDate,
+    );
+
+    // Both lists are empty
+    if (f460Committees.length < 1 && f496Committees.length < 1) return [];
+
+    // Both lists have items
+    if (f460Committees.length > 0 && f496Committees.length > 0) {
+      const combinedList = this.combineCommitteeLists(
+        f460Committees,
+        f496Committees,
+      );
+      return combinedList;
+    }
+
+    // Only one list has items
+    return [...f460Committees, ...f496Committees];
+  }
+
+  combineCommitteeLists(
+    list1: IndependentExpenditureCommittee[],
+    list2: IndependentExpenditureCommittee[],
+  ): IndependentExpenditureCommittee[] {
+    const committeeMap = new Map();
+
+    list1.forEach((committee) =>
+      committeeMap.set(committee.committeeName, committee),
+    );
+
+    list2.forEach((committee) => {
+      const foundCommittee = committeeMap.get(committee.committeeName);
+      if (foundCommittee) {
+        const newSum = foundCommittee.sum + committee.sum;
+        const newCommittee = { ...foundCommittee, sum: newSum };
+        committeeMap.set(foundCommittee.committeeName, newCommittee);
+      }
+    });
+
+    return Array.from(committeeMap.values());
   }
 
   async getIndependentExpenditureCommittees(
     candidateName: string,
     supOppCd: string,
     electionDate: string,
-  ) {
+  ): Promise<IndependentExpenditureCommittee[]> {
     const query = this.connection
       .getRepository(EXPNEntity)
       .createQueryBuilder()
@@ -69,6 +130,50 @@ export class IndependentExpenditureCommitteesService {
       .orderBy('sum', 'DESC');
 
     const committees = await query.getRawMany();
-    return committees;
+    return committees.map((committee) => ({
+      ...committee,
+      sum: parseInt(committee.sum),
+    }));
+  }
+
+  async getLateIndependentExpenditureCommittees(
+    candidateName: string,
+    supOppCd: string,
+    electionDate: string,
+  ): Promise<IndependentExpenditureCommittee[]> {
+    const query = this.connection
+      .getRepository(S496Entity)
+      .createQueryBuilder()
+      .select('filer_naml', 'committeeName')
+      .addSelect(`json_build_object ('name', filer_naml)`, 'committee')
+      .addSelect('ROUND(SUM(amount))', 'sum')
+
+      .where('cand_naml iLike :candidateName', {
+        candidateName: `%${candidateName}%`,
+      })
+      .andWhere('rec_type = :recType', { recType: 'S496' })
+      .andWhere('form_type IN (:...formType)', { formType: ['F496'] })
+      .andWhere('supp_opp_cd = :supOppCd', { supOppCd: supOppCd })
+      .andWhere(
+        `to_date(exp_date, 'YYYYMMDD') >= to_date(:electionDate, 'MM/DD/YYYY') - interval '${this.months} month'`,
+        {
+          electionDate,
+        },
+      )
+      .andWhere(
+        `to_date(exp_date, 'YYYYMMDD') <= to_date(:electionDate , 'MM/DD/YYYY')`,
+        {
+          electionDate,
+        },
+      )
+
+      .groupBy('filer_naml')
+      .orderBy('sum', 'DESC');
+
+    const committees = await query.getRawMany();
+    return committees.map((committee) => ({
+      ...committee,
+      sum: parseInt(committee.sum),
+    }));
   }
 }
--- libs/sdvv-database/src/independent-expenditures/Independent-expenditure-sums/independent-expenditures.service.ts
+++ libs/sdvv-database/src/independent-expenditures/Independent-expenditure-sums/independent-expenditures.service.ts
@@ -1,3 +1,4 @@
+import { S496Entity } from '@app/sdvv-database/tables-xlsx/s496/s496.entity';
 import { Injectable } from '@nestjs/common';
 import { Connection } from 'typeorm';
 import { EXPNEntity } from '../../tables-xlsx/expn/expn.entity';
@@ -17,28 +18,47 @@ export class IndependentExpendituresService {
   async supportSum(candidateName: string, electionYear: string) {
     const yearEndDate = `${this.monthDay}/${electionYear}`;
 
-    return this.getIndependentExpendituresSum(
+    const f460Sum = await this.getIndependentExpendituresSum(
       candidateName,
       'SUPPORT',
       yearEndDate,
     );
+
+    const f496Sum = await this.getLateIndependentExpendituresSum(
+      candidateName,
+      'SUPPORT',
+      yearEndDate,
+    );
+
+    return f460Sum + f496Sum;
   }
 
-  async opposeSum(candidateName: string, electionYear: string) {
+  async opposeSum(
+    candidateName: string,
+    electionYear: string,
+  ): Promise<number> {
     const yearEndDate = `${this.monthDay}/${electionYear}`;
 
-    return this.getIndependentExpendituresSum(
+    const f460Sum = await this.getIndependentExpendituresSum(
       candidateName,
       'OPPOSE',
       yearEndDate,
     );
+
+    const f496Sum = await this.getLateIndependentExpendituresSum(
+      candidateName,
+      'OPPOSE',
+      yearEndDate,
+    );
+
+    return f460Sum + f496Sum;
   }
 
   async getIndependentExpendituresSum(
     candidateName: string,
     supOppCd: string,
     electionDate: string,
-  ) {
+  ): Promise<number> {
     const { sum: expSum } = await this.connection
       .getRepository(EXPNEntity)
       .createQueryBuilder()
@@ -64,6 +84,39 @@ export class IndependentExpendituresService {
       )
       .getRawOne();
 
-    return expSum ? expSum : 0;
+    return expSum ? parseInt(expSum) : 0;
+  }
+
+  async getLateIndependentExpendituresSum(
+    candidateName: string,
+    supOppCd: string,
+    electionDate: string,
+  ): Promise<number> {
+    const { sum: expSum } = await this.connection
+      .getRepository(S496Entity)
+      .createQueryBuilder()
+      .select('COALESCE(ROUND(SUM(amount)), 0)', 'sum')
+
+      .where('cand_naml iLike :candidateName', {
+        candidateName: `%${candidateName}%`,
+      })
+      .andWhere('rec_type = :recType', { recType: 'S496' })
+      .andWhere('form_type IN (:...formType)', { formType: ['F496'] })
+      .andWhere('supp_opp_cd = :supOppCd', { supOppCd: supOppCd })
+      .andWhere(
+        `to_date(exp_date, 'YYYYMMDD') >= to_date(:electionDate, 'MM/DD/YYYY') - interval '${this.months} month'`,
+        {
+          electionDate,
+        },
+      )
+      .andWhere(
+        `to_date(exp_date, 'YYYYMMDD') <= to_date(:electionDate , 'MM/DD/YYYY')`,
+        {
+          electionDate,
+        },
+      )
+      .getRawOne();
+
+    return expSum ? parseInt(expSum) : 0;
   }
 }
