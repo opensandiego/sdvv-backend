e158b5b3c3732acc7799819655fad8cce55e263d
Robert <robertgz@users.noreply.github.com>
2023-03-27 22:42:00 -0700

    Reverts Redis connections to use REDIS_URL. Adds password to Redis container and connection URL in Docker Compose.
    resolves #100

--- libs/sdvv-database/src/last-update/last-update.module.ts
+++ libs/sdvv-database/src/last-update/last-update.module.ts
@@ -1,6 +1,5 @@
 import { CacheModule, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { redisStore } from 'cache-manager-ioredis-yet';
 import { LastUpdateResolver } from './last-update.resolver';
 
 const SIX_HOURS = 21600000; // milliseconds
@@ -10,12 +9,9 @@ const TEN_SECONDS = 10000; // milliseconds
   imports: [
     CacheModule.registerAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        store: await redisStore({
-          host: configService.get('redis.host'),
-          port: configService.get('redis.port'),
-          ttl: process.env.NODE_ENV === 'production' ? SIX_HOURS : TEN_SECONDS,
-        }),
+      useFactory: async () => ({
+        url: process.env.REDIS_URL,
+        ttl: process.env.NODE_ENV === 'production' ? SIX_HOURS : TEN_SECONDS,
       }),
       inject: [ConfigService],
     }),
